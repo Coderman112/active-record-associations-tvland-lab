432e639a9485b4bbfef31fbde2b60f804e3dca7a
ChristianCudby <christiancudby@hotmail.com>
2021-02-21 20:39:25 -0500

    commit tree c5d08bbe3877686e8344b468016c481cf4b2111b

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.collect do |t|
+            "#{t.first_name} #{t.last_name}"
+        end
+    end
+
+    def characters_array
+        self.characters.collect do |t|
+            t.name
+        end
+    end
   
 end
